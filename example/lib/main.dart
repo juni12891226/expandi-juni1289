8586ee5e82f33f02be26548deaf62c9e214cfd88
Junaid Hassan <junaid.hassan@evampsaanga.com>
2024-10-17 16:46:15 +0500

    package updated with smooth animation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:expandi_juni1289/expandi_juni1289.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,24 +28,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool expanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsetsDirectional.only(top: 100),
-        child: Expandi(
-          expandableChildAnimationMilliSecondsDuration: 300,//optional
-          onExpandCollapseCallback: (bool isExpanded) {
-            setState(() {
-              expanded = !isExpanded;
-            });
-          },
-          isExpanded: expanded,
-          headerWidget: Container(
-            padding: const EdgeInsets.only(top: 20, bottom: 20, right: 20),
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            color: Colors.black,
-            child: const Row(
+        body: Container(
+          margin: const EdgeInsetsDirectional.only(top: 100),
+          child: Expandi(
+            expandableChildAnimationMilliSecondsDuration: 500,
+            expandableIconAnimationMilliSecondsDuration: 1,
+            //optional
+            onExpandCollapseCallback: (bool isExpanded) {
+              setState(() {
+                expanded = !isExpanded;
+              });
+            },
+            marginBetweenExpandableIcon: 40,
+            isExpanded: expanded,
+            headerContainerDecoration: const BoxDecoration(color: Colors.yellow),
+            headerContainerMargin: const EdgeInsetsDirectional.all(40),
+            headerContainerPadding: const EdgeInsetsDirectional.all(40),
+
+            headerWidget: const Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -59,18 +62,136 @@ class _MyHomePageState extends State<MyHomePage> {
                     ))
               ],
             ),
-          ),
-          expandableChild: Container(
-            padding: const EdgeInsets.only(top: 20, bottom: 20, left: 12, right: 12),
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            color: Colors.blue,
-            child: const Text(
-              "Hello world, greetings from the expandi, this would be a long journey!",
-              style: TextStyle(color: Colors.white),
+            expandableChild: Container(
+              padding: const EdgeInsets.only(top: 20, bottom: 20, left: 12, right: 12),
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              color: Colors.blue,
+              child: const Text(
+                "Hello world, greetings from the expandi, this would be a long journey!",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
-        ),
-      )
+        )
     );
   }
 }
+
+
+class Expandi extends StatefulWidget {
+  ///if widget is expanded or not
+  final bool isExpanded;
+
+  ///widget below the header widget
+  ///expandable widget
+  final Widget expandableChild;
+
+  ///top header
+  ///non expandable widget
+  final Widget headerWidget;
+
+  ///expandable child animation duration default is 500 ms
+  final int expandableChildAnimationMilliSecondsDuration;
+
+  ///icon animation duration default is 300 ms
+  final int expandableIconAnimationMilliSecondsDuration;
+
+  ///callback fired, when the widget
+  ///change its state from collapse to expand and vice versa
+  final Function(bool isExpanded) onExpandCollapseCallback;
+
+  ///margin between the header widget and the icon (if provided)
+  final double marginBetweenExpandableIcon;
+
+  ///expandable icon widget can by any widget (icon)
+  final Widget? expandableIconWidget;
+
+  final BoxDecoration? headerContainerDecoration;
+  final EdgeInsetsDirectional? headerContainerPadding;
+  final EdgeInsetsDirectional? headerContainerMargin;
+
+  const Expandi({Key? key,
+    this.expandableIconWidget,
+    this.headerContainerMargin,
+    this.headerContainerPadding,
+    this.headerContainerDecoration,
+    this.marginBetweenExpandableIcon = 0,
+    required this.onExpandCollapseCallback,
+    required this.headerWidget,
+    required this.isExpanded,
+    required this.expandableChild,
+    this.expandableChildAnimationMilliSecondsDuration = 1000,
+    this.expandableIconAnimationMilliSecondsDuration = 500})
+      : super(key: key);
+
+  @override
+  State<Expandi> createState() => _ExpandiState();
+}
+
+class _ExpandiState extends State<Expandi> with TickerProviderStateMixin {
+  late AnimationController controller;
+ late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration:  Duration(milliseconds: widget.expandableChildAnimationMilliSecondsDuration),
+      vsync: this,
+    );
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.fastLinearToSlowEaseIn,
+    );
+  }
+
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      GestureDetector(
+        onTap: () {
+          widget.onExpandCollapseCallback(widget.isExpanded);
+          if (animation.status != AnimationStatus.completed) {
+            controller.forward();
+          } else {
+            controller.animateBack(0, duration: Duration(milliseconds: widget.expandableChildAnimationMilliSecondsDuration));
+          }
+        },
+        child: Container(
+          decoration: widget.headerContainerDecoration,
+          padding: widget.headerContainerPadding,
+          margin: widget.headerContainerMargin,
+          child: Row(mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Expanded(
+                child: widget.headerWidget),
+
+            SizedBox(width: widget.marginBetweenExpandableIcon),
+            GestureDetector(
+                onTap: () {
+                  widget.onExpandCollapseCallback(widget.isExpanded);
+                },
+                child: AnimatedRotation(
+                  turns: widget.isExpanded ? 0.5 : 0,
+                  duration: Duration(milliseconds: widget.expandableIconAnimationMilliSecondsDuration),
+                  child: widget.expandableIconWidget ?? const Icon(Icons.keyboard_arrow_down, size: 16, color: Colors.purple,),
+                )),
+          ]),
+        ),
+      ),
+      SizeTransition(
+        sizeFactor: animation,
+        axis: Axis.vertical,
+        child:Container(child: widget.expandableChild)
+        ),
+
+    ]);
+  }
+}
+
--- lib/expandi_juni1289.dart
+++ lib/expandi_juni1289.dart
@@ -28,73 +28,89 @@ class Expandi extends StatefulWidget {
   ///expandable icon widget can by any widget (icon)
   final Widget? expandableIconWidget;
 
-  ///padding to the end of header widget
-  final double headerWidgetEndPadding;
+  final BoxDecoration? headerContainerDecoration;
+  final EdgeInsetsDirectional? headerContainerPadding;
+  final EdgeInsetsDirectional? headerContainerMargin;
 
   const Expandi(
       {Key? key,
       this.expandableIconWidget,
+      this.headerContainerMargin,
+      this.headerContainerPadding,
+      this.headerContainerDecoration,
       this.marginBetweenExpandableIcon = 0,
       required this.onExpandCollapseCallback,
       required this.headerWidget,
       required this.isExpanded,
       required this.expandableChild,
-      this.expandableChildAnimationMilliSecondsDuration = 850,
-      this.expandableIconAnimationMilliSecondsDuration = 500,
-      this.headerWidgetEndPadding = 0})
+      this.expandableChildAnimationMilliSecondsDuration = 1000,
+      this.expandableIconAnimationMilliSecondsDuration = 500})
       : super(key: key);
 
   @override
   State<Expandi> createState() => _ExpandiState();
 }
 
-class _ExpandiState extends State<Expandi> with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
+class _ExpandiState extends State<Expandi> with TickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
+    controller = AnimationController(
+      duration: Duration(milliseconds: widget.expandableChildAnimationMilliSecondsDuration),
       vsync: this,
-      duration: Duration(milliseconds: widget.expandableIconAnimationMilliSecondsDuration),
-      upperBound: 0.5,
+    );
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.fastLinearToSlowEaseIn,
     );
   }
 
   @override
   void dispose() {
-    _controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      Row(mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center, children: [
-        Expanded(
-            child: GestureDetector(
+      GestureDetector(
+        onTap: () {
+          widget.onExpandCollapseCallback(widget.isExpanded);
+          if (animation.status != AnimationStatus.completed) {
+            controller.forward();
+          } else {
+            controller.animateBack(0, duration: Duration(milliseconds: widget.expandableChildAnimationMilliSecondsDuration));
+          }
+        },
+        child: Container(
+          decoration: widget.headerContainerDecoration,
+          padding: widget.headerContainerPadding,
+          margin: widget.headerContainerMargin,
+          child: Row(mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Expanded(child: widget.headerWidget),
+            SizedBox(width: widget.marginBetweenExpandableIcon),
+            GestureDetector(
                 onTap: () {
                   widget.onExpandCollapseCallback(widget.isExpanded);
                 },
-                child: widget.headerWidget)),
-
-          SizedBox(width: widget.marginBetweenExpandableIcon),
-          GestureDetector(
-              onTap: () {
-                widget.onExpandCollapseCallback(widget.isExpanded);
-              },
-              child: AnimatedRotation(
-                turns: widget.isExpanded ? 0.5 : 0,
-                duration:  Duration(milliseconds:widget.expandableIconAnimationMilliSecondsDuration),
-                child: widget.expandableIconWidget??const Icon(Icons.keyboard_arrow_down,size: 16,color: Colors.purple,),
-              )),
-          SizedBox(width: widget.headerWidgetEndPadding)
-
-      ]),
-      AnimatedSize(
-          duration: Duration(milliseconds: widget.expandableChildAnimationMilliSecondsDuration),
-          curve: Curves.fastOutSlowIn,
-          child: widget.isExpanded ? widget.expandableChild : const SizedBox(height: 0, width: 0))
+                child: AnimatedRotation(
+                  turns: widget.isExpanded ? 0.5 : 0,
+                  duration: Duration(milliseconds: widget.expandableIconAnimationMilliSecondsDuration),
+                  child: widget.expandableIconWidget ??
+                      const Icon(
+                        Icons.keyboard_arrow_down,
+                        size: 16,
+                        color: Colors.purple,
+                      ),
+                )),
+          ]),
+        ),
+      ),
+      SizeTransition(sizeFactor: animation, axis: Axis.vertical, child: Container(child: widget.expandableChild)),
     ]);
   }
 }
